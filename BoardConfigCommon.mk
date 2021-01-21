d411614d847819ac7c4e0819ff43efb9ce6a1f12
doc HD <doc.divxm@gmail.com>
2021-01-21 13:50:51 +0300

    sm8250-common: Switch to common Neutrino kernel
    Change-Id: I253fbfea2e11731c9b8a446981a00df1fff54163

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,8 +56,11 @@ NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
+TARGET_KERNEL_CONFIG := neutrino_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86_proton/clang-proton
+KERNEL_SUPPORTS_LLVM_TOOLS := true
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
