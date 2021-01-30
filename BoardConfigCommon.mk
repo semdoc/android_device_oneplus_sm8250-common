8051ba56e40d9c93e73a451f7a0e948e064fe1dd
doc HD <doc.divxm@gmail.com>
2021-01-30 09:56:52 +0300

    Build kernel with latest clang from custom repo
    Change-Id: I17f38726988e7291996428e2fa2469745068e06a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,9 +57,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CONFIG := neutrino_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86_proton/clang-proton
+TARGET_KERNEL_CLANG_VERSION := latest
 KERNEL_SUPPORTS_LLVM_TOOLS := true
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
